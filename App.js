5af2cab09352c0bc3a8d42d51ce65dbed95fcdd7
moideen18 <158563065+moideen18@users.noreply.github.com>
2025-01-30 16:01:21 +0530

    App.js

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./component/Register";
+import Register from "./component/Register"; // Ensure correct path
+import Login from "./component/Login";
 import Tasks from "./component/Tasks";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Register />} />
-
-        
+        <Route path="/login" element={<Login />} />
         <Route path="/tasks" element={<Tasks />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
